5ec6b29184249fe9588f4e10cc3e5b4286095f11
mounima <saimounima@gmail.com>
2017-09-14 15:12:15 -0500

    HW1-CSCE-606
    Committing homework 1 for csce 606 - UIN-625001179

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,14 +3,26 @@
 # Part 1
 
 def sum arr
+
+   arr.reduce 0, :+
   # YOUR CODE HERE
 end
 
 def max_2_sum arr
+sum(arr.sort.last(2))
   # YOUR CODE HERE
 end
 
 def sum_to_n? arr, n
+if arr.empty?
+    return false if n == 0
+  else
+    arr.combination(2).to_a.each do |pair|
+      return true if sum(pair) == n
+    end
+  end
+
+  return false
   # YOUR CODE HERE
 end
 
@@ -18,13 +30,21 @@ end
 
 def hello(name)
   # YOUR CODE HERE
+    "Hello, " + name
 end
 
 def starts_with_consonant? s
+!!(s[0] =~ /[bcdfghjklmnprstvwxyz]+/i)
   # YOUR CODE HERE
 end
 
 def binary_multiple_of_4? s
+  return false if s.empty?
+    s.each_char{ |c|
+        return false if c != "1" and c != "0"}
+    return true if s.to_i(2) % 4 == 0
+    false # false in any other case
+
   # YOUR CODE HERE
 end
 
@@ -32,4 +52,15 @@ end
 
 class BookInStock
 # YOUR CODE HERE
+  attr_accessor :isbn, :price
+
+  def initialize isbn, price
+    raise ArgumentError if isbn.empty? || price <= 0
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    format("$%.2f", @price)
+  end
 end
